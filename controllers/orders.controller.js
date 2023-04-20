455afeb58125bde443fc083e059b0cebd3c46484
jackerbell <jackerbell@naver.com>
2023-04-20 17:47:40 +0900

    장바구니 기능, 주문 기능 추가 및 기타 오류 수정

---
+++ controllers/orders.controller.js
@@ -0,0 +1,58 @@
+const stripe = require('stripe')('sk_test_51Myrw1CRXS1yysnoXTTzxpqivTUaSASszdAhrXUOtXHq8iH8mC9Gn8ktTRcVNgc6oHzrHGw82KmcllA6nOTZsa45008M6yPUMD');
+
+const Order = require('../models/order.model');
+const User = require('../models/user.model');
+
+async function getOrders(req, res) {
+  try {
+    const orders = await Order.findAllForUser(res.locals.uid);
+    res.render('customer/orders/all-orders', {
+      orders: orders
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function addOrder(req, res, next) {
+  const cart = res.locals.cart;
+
+  let userDocument;
+  try {
+    userDocument = await User.findById(res.locals.uid);
+  } catch (error) {
+    return next(error);
+  }
+
+  const order = new Order(cart, userDocument);
+
+  try {
+    await order.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  req.session.cart = null;
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: '{{PRICE_ID}}',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.redirect(303, session.url);
+  // res.redirect('/orders');
+}
+
+module.exports = {
+  addOrder: addOrder,
+  getOrders: getOrders,
+};
--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -2,7 +2,7 @@ const cartItemUpdateFormElements = document.querySelectorAll(
   '.cart-item-management'
 );
 const cartTotalPriceElement = document.getElementById('cart-total-price');
-const cartBadge = document.querySelector('.nav-items .badge');
+const cartBadgeElements = document.querySelectorAll('.nav-items .badge');
 
 async function updateCartItem(event) {
   event.preventDefault();
@@ -38,19 +38,22 @@ async function updateCartItem(event) {
 
   const responseData = await response.json();
 
-  if(responseData.updatedCartData.updatedItemPrice === 0){
+  if (responseData.updatedCartData.updatedItemPrice === 0) {
     form.parentElement.parentElement.remove();
-  }else {
+  } else {
     const cartItemTotalPriceElement =
       form.parentElement.querySelector('.cart-item-price');
     cartItemTotalPriceElement.textContent =
-      responseData.updatedCartData.updatedItemPrice.toFixed(2);    
+      responseData.updatedCartData.updatedItemPrice.toFixed(2);
   }
-  
+
   cartTotalPriceElement.textContent =
     responseData.updatedCartData.newTotalPrice.toFixed(2);
 
-  cartBadge.textContent = responseData.updatedCartData.newTotalQuantity;
+  for (const cartBadgeElement of cartBadgeElements) {
+    cartBadgeElement.textContent =
+      responseData.updatedCartData.newTotalQuantity;
+  }
 }
 
 for (const formElement of cartItemUpdateFormElements) {
