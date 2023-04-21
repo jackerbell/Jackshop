8cbc39d79838d9f065a1abb247db4b8ae24fceff
jackerbell <jackerbell@naver.com>
2023-04-21 17:46:11 +0900

    결제(스트라이프)기능 추가 및 프로젝트 완료 배포 전 검토 예정

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,6 +1,5 @@
-const stripe = require('stripe')('sk_test_51Myrw1CRXS1yysnoXTTzxpqivTUaSASszdAhrXUOtXHq8iH8mC9Gn8ktTRcVNgc6oHzrHGw82KmcllA6nOTZsa45008M6yPUMD');
-
-const Order = require('../models/order.model');
+const stripe = require('stripe')('sk_test_51Myrw1CRXS1yysnoXTTzxpqivTUaSASszdAhrXUOtXHq8iH8mC9Gn8ktTRcVNgc6oHzrHGw82KmcllA6nOTZsa45008M6yPUMD'); // 결제 관련 이슈(시크릿 코드를 사용한 실제 요금 청구)로 인해 문자열 처리, 
+const Order = require('../models/order.model');                         //사용하려는 경우에는 스프라이트 가입 후 발행한 시크릿 키를 넣어 사용가능
 const User = require('../models/user.model');
 
 async function getOrders(req, res) {
@@ -36,23 +35,40 @@ async function addOrder(req, res, next) {
   req.session.cart = null;
 
   const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
+    payment_method_types:['card'],
+    line_items: cart.items.map(function(item){
+      return {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
-        quantity: 1,
-      },
-    ],
+        price_data: {
+          currency: 'usd',
+          product_data:{
+            name: item.product.title
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100 // stripe 계산식으로 인해 소수점 두 자리 이상으로 변환, 가격을 센트로 변환
+        },
+        quantity: item.quantity,
+      } 
+    }),
     mode: 'payment',
-    success_url: `${YOUR_DOMAIN}?success=true`,
-    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
   });
 
   res.redirect(303, session.url);
   // res.redirect('/orders');
 }
 
+function getSuccess(req,res) {
+  res.render('customer/orders/success');
+}
+
+function getFailure(req,res) {
+  res.render('customer/orders/failure');
+}
+
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure
 };
